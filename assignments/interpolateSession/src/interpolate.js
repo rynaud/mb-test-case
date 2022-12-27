62a38601335739ac0029049a7627040405cfa008
Naci Aytı <naciayti@massivebioinformatics.com>
2022-12-27 17:04:55 +0300

    interpolate complated

---
+++ assignments/interpolateSession/src/interpolate.js
@@ -0,0 +1,11 @@
+const interpolate = (value, session = {}, options = {}) => {
+  var regexp = new RegExp(
+    `${options.leftDelimiter}([^${options.rightDelimiter}]+)${options.rightDelimiter}`,
+    "g"
+  );
+  return value.replace(regexp, function (ignore, key) {
+    return (key = session[key]) == null ? "" : key;
+  });
+};
+
+module.exports = { interpolate };
